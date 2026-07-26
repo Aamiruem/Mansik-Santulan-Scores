40aac2a10c2d5c06ae15b73591f80a4b78e9a91f
Aamir Hussain <aamirhussainazad786@gmail.com>
2026-07-26 12:40:40 +0530

    commit add

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // (() => {
 //   "use strict";
 
-//   const API_BASE = "https://mansik-santulan-score.onrender.com";
+//   const API_BASE = "https://mansik-santulan-scores.onrender.com";
 
 //   const form = document.getElementById("predict-form");
 //   const submitBtn = document.getElementById("submit-btn");
@@ -317,7 +317,7 @@
 (() => {
   "use strict";
 
-  const API_BASE = "https://mansik-santulan-score.onrender.com";
+  const API_BASE = "https://mansik-santulan-scores.onrender.com";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
